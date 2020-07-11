638792903d6ff412a0db401f85a34ab769939246
Sergey <example@gmail.com>
2020-07-11 15:15:08 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 "use strict";
 
 console.log('arr' + " - object");
-console.log(4 + " - object");
+console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+console.log(++incr);    
+console.log(--decr);
+
+console.log(2 * 3 === '6');
